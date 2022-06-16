95f7a92810851145282e21d2d19bccc909d58598
Juan Sterren <codenv.sterren@gmail.com>
2022-06-16 18:58:19 -0300

    microservice-users complete

--- 03-microservices-superflights/api-gateway/src/app.module.ts
+++ 03-microservices-superflights/api-gateway/src/app.module.ts
@@ -3,8 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { FlightModule } from './flight/flight.module';
 import { PassengerModule } from './passenger/passenger.module';
+import { FlightModule } from './flight/flight.module';
 
 @Module({
   imports: [
@@ -13,8 +13,8 @@ import { PassengerModule } from './passenger/passenger.module';
       isGlobal: true,
     }),
     UserModule,
-    FlightModule,
-    PassengerModule
+    PassengerModule,
+    FlightModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- 03-microservices-superflights/api-gateway/src/main.ts
+++ 03-microservices-superflights/api-gateway/src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from './common/filters/http-exception.filter';
 import { TimeOutInterceptor } from './common/interceptors/timeout.interceptor';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,17 +11,20 @@ async function bootstrap() {
 
   const options = new DocumentBuilder()
     .setTitle('SuperFlight API')
-    .setDescription('Scheduled Flight APP')
+    .setDescription('Scheduled Flights App')
     .setVersion('2.0.0')
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, options);
+
   SwaggerModule.setup('/api/docs', app, document, {
     swaggerOptions: {
       filter: true,
-    }
+    },
   });
 
   await app.listen(process.env.PORT || 3000);
+  console.log('API Gateway is listening');
 }
 bootstrap();
