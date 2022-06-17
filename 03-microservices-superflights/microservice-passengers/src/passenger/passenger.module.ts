aae14a5d94ef867cbd9bd60fe90d877ae2227c60
Juan Sterren <codenv.sterren@gmail.com>
2022-06-17 00:01:17 -0300

    microservice-passengers complete

--- 03-microservices-superflights/microservice-passengers/src/passenger/passenger.module.ts
+++ 03-microservices-superflights/microservice-passengers/src/passenger/passenger.module.ts
@@ -16,6 +16,5 @@ import { PassengerSchema } from './schema/passenger.schema';
   ],
   controllers: [PassengerController],
   providers: [PassengerService],
-  exports: [PassengerService]
 })
 export class PassengerModule { }
